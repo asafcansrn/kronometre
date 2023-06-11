2e7ca2447ac2265bb7874963d9e11768ac94e284
asafcansrn <asafcansrn@gmail.com>
2023-06-11 15:43:47 +0300

    Mobil uygulama kronometre ödevi

---
+++ app/src/main/java/com/asafcansrn/kronometre/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.asafcansrn.kronometre
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
+import com.asafcansrn.kronometre.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var stopTime: Long = 0
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnStart.setOnClickListener {
+            binding.kronometre.base = SystemClock.elapsedRealtime() + stopTime
+            binding.kronometre.start()
+            binding.btnStart.visibility = View.GONE
+            binding.btnPause.visibility = View.VISIBLE
+            binding.imageStart.setImageDrawable(getDrawable(R.drawable.pause))
+        }
+
+        binding.btnPause.setOnClickListener {
+            stopTime = binding.kronometre.base - SystemClock.elapsedRealtime()
+            binding.kronometre.stop()
+            binding.btnStart.visibility = View.VISIBLE
+            binding.btnPause.visibility = View.GONE
+            binding.imageStart.setImageDrawable(getDrawable(R.drawable.start))
+        }
+
+        binding.btnReset.setOnClickListener {
+            binding.kronometre.base = SystemClock.elapsedRealtime()
+            binding.kronometre.stop()
+            stopTime = 0
+            binding.btnStart.visibility = View.VISIBLE
+            binding.btnPause.visibility = View.GONE
+            binding.imageStart.setImageDrawable(getDrawable(R.drawable.start))
+        }
+    }
+}
